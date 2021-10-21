74954b536ab5c4dad8fa4eea3671d027e6699756
StevenHelin <55350189+StevenHelin@users.noreply.github.com>
2021-10-21 11:09:41 +0200

    2nd save

--- src/Function/CSPGenerator.java
+++ src/Function/CSPGenerator.java
@@ -3,13 +3,15 @@ package Function;
 
 import CSP.CSP;
 
+
+
 public class CSPGenerator {
     int n; //nb de variable
     int c; //nb de contraintes
     int it; //nb d'iteration
     int d; //domaine
     int[] nbSolution; //nb de solution
-    CSP csp= new CSP();
+    //CSP csp= new CSP();
 
     public void Generator(int n, int c,int d  ){
         int nbv=n; //nb de variable
